dc53778edd35b5975161c61df101fc115abe83e2
Udhay <udhay1415@gmail.com>
2017-10-31 14:02:20 +0530

    initial commit

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,20 +1,25 @@
 import React, { Component} from 'react';
-import { Text,View,TouchableWithoutFeedback } from 'react-native';
+import { Text,View,TouchableWithoutFeedback,LayoutAnimation,UIManager } from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component{
-  
+  componentWillUpdate(){
+    UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true);
+    LayoutAnimation.spring();
+  }
   // Helper function
   // Displays the description, based on the expand object returned from mapStateToProps
   renderDescription(){
     const { library, expanded } = this.props;
     if(expanded) {
       return(
-        <Text>
-          {library.description}
-        </Text>
+        <CardSection>
+          <Text style={{ flex:1 }}>
+            {library.description}
+          </Text>
+        </CardSection>
       );
     }
   }
@@ -53,7 +58,7 @@ const mapStateToProps = (state, ownProps) => {
   const expanded = state.selectedLibraryId === ownProps.library.id;
 // mapStateToProps must always return an object
   return { expanded : expanded };
-}
+};
 
 // actions and states are sent to the component using connect
 export default connect(mapStateToProps,actions)(ListItem);
